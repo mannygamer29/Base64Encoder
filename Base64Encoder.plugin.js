faa70e92a1d3c98eeaed4bdf9e132a5717ec004e
manny :3 <146687033+mannygamer29@users.noreply.github.com>
2025-02-22 18:55:08 +1100

    Add files via upload

---
+++ Base64Encoder.plugin.js
@@ -0,0 +1,48 @@
+/**
+ * @name Base64Encoder
+ * @author mannygamer29
+ * @description Encodes outgoing messages into Base64 automatically.
+ * @version 1.0.1
+ * @authorLink https://yourlinkhere.com
+ * @website https://yourwebsitehere.com
+ * @source https://github.com/yourrepohere
+ */
+
+class Base64Encoder {
+    start() {
+        this.originalSendMessage = BdApi.findModuleByProps('sendMessage').sendMessage;
+        this.patchSendMessage();
+        BdApi.showToast('Base64 Encoder plugin started!');
+    }
+
+    stop() {
+        this.unpatchSendMessage();
+        BdApi.showToast('Base64 Encoder plugin stopped.');
+    }
+
+    patchSendMessage() {
+        const messageModule = BdApi.findModuleByProps('sendMessage');
+        const sendMessage = messageModule.sendMessage;
+
+        messageModule.sendMessage = (channelId, message, ...args) => {
+            if (message && typeof message.content === 'string') {
+                try {
+                    const encodedContent = btoa(unescape(encodeURIComponent(message.content)));
+                    message.content = encodedContent;
+                } catch (err) {
+                    console.error('Failed to encode message:', err);
+                }
+            }
+            return sendMessage.call(messageModule, channelId, message, ...args);
+        };
+    }
+
+    unpatchSendMessage() {
+        const messageModule = BdApi.findModuleByProps('sendMessage');
+        if (this.originalSendMessage) {
+            messageModule.sendMessage = this.originalSendMessage;
+        }
+    }
+}
+
+module.exports = Base64Encoder;
